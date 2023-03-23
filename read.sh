3ec6359b2df98468a262bfd8ed0d3b04802043fa
nhokcherry <baosuper456@gmail.com>
2023-03-23 08:56:02 +0700

    fix bash script

--- read.sh
+++ read.sh
@@ -2,15 +2,24 @@
 
 echo -e "Which file you want to modify: \c"
 read filename
+
 if [[ -f $filename ]]
 then
     echo -e "Enter the word you want to replace: \c"
     read word
+    
     if grep -q $word $filename
     then
+        # Thay tu minh muon thay vao
         echo -e "Enter the word you want: \c"
         read replace
-        sed  "s/$word/$replace/g" $filename
+        sed -i "s/$word/$replace/g" $filename
+
+        # Doc file ra man hinh
+        cat $filename | while read p
+        do
+            echo $p
+        done  
     else
         echo "The word that you type doesn't exist"
     fi
